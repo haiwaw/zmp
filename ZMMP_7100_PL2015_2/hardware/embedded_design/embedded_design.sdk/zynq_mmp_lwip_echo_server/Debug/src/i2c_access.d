6a0ce4b54516cf4928bc686f5cd282aaf643da4f
haiwawwang <haiwawwang@tencent.com>
2018-04-23 12:01:21 +0800

    delete

--- ZMMP_7100_PL2015_2/hardware/embedded_design/embedded_design.sdk/zynq_mmp_lwip_echo_server/Debug/src/i2c_access.d
+++
@@ -1,7 +0,0 @@
-src/i2c_access.o src/i2c_access.d: ../src/i2c_access.c \
- ../../zynq_mmp_lwip_echo_server_bsp/ps7_cortexa9_0/include/xparameters.h \
- ../../zynq_mmp_lwip_echo_server_bsp/ps7_cortexa9_0/include/xparameters_ps.h
-
-../../zynq_mmp_lwip_echo_server_bsp/ps7_cortexa9_0/include/xparameters.h:
-
-../../zynq_mmp_lwip_echo_server_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
--- ZMMP_7100_PL2015_2/petalinux_qemu.sh
+++
@@ -1,3 +0,0 @@
-#！ /bin/sh
-petalinux-boot --qemu --kernel --qemu-args "-redir tcp:10021:10.0.2.15:21 -redir tcp:10023:10.0.2.15:23 -redir tcp:10080:10.0.2.15:80\
-        -redir tcp:10022:10.0.2.15:22"
